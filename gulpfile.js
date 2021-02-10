79448f45961dad8004a3952a04100051b270aeba
erguotou <erguotou525@gmail.com>
2021-02-10 15:47:21 +0800

    feat: 手动图片压缩

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,14 @@ const assetsPath = path.resolve(__dirname, 'dist/assets/');
 task('image', () => {
   return src(path.resolve(__dirname, 'src/assets/*.*'))
     .pipe(rev())
-    .pipe(
-      imageMin({
-        optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
-        progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
-        interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-        multipass: true, //类型：Boolean 默认：false 多次优化svg直到完全优化
-      })
-    )
+    // .pipe(
+    //   imageMin({
+    //     optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+    //     progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+    //     interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+    //     multipass: true, //类型：Boolean 默认：false 多次优化svg直到完全优化
+    //   })
+    // )
     .pipe(dest(assetsPath))
     .pipe(rev.manifest())
     .pipe(dest(assetsPath));
